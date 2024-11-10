c6624d097672613d76c078b69918545f31507b55
kyousuf88 <kanwalyousuf307@gmail.com>
2024-11-11 00:12:35 +0500

    new changes for deployment in vercel

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from "react"
+import Navbar from "./Navbar"
 
 const Hero = () => {
   return (
-    <div id='hero' className='min-h-screen bg-no-repeat bg-[url(/kanwal.jpg)]  bg-cover'
+    <div id="hero" className="min-h-screen bg-no-repeat bg-[url(/kanwal.jpg)]  bg-cover"
     style={{backgroundSize: "35%", backgroundPosition:"left 100px top 100px"}}
     >
       <Navbar />
-      <div className='container grid lg:grid-cols-2 h-[calc(100vh-60px)]'>
-        <div className='hidden lg:block'></div>
-        <div className='text-[80px] sm:text-[100px] font-bold leading-tight flex justify-center items-center'>
+      <div className="container grid lg:grid-cols-2 h-[calc(100vh-60px)]">
+        <div className="hidden lg:block"></div>
+        <div className="text-[80px] sm:text-[100px] font-bold leading-tight flex justify-center items-center">
           <div>
             <p data-aos="zoom-in-up">I'm</p>
             <p data-aos="zoom-in-up">Kanwal</p>
